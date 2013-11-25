7bfe0c4ef2db5bd66b6a1163e406c50e6c39e4db
Robert Mosolgo <rdmosolgo@gmail.com>
2013-11-25 12:36:54 -0800

    fix batman app in precompile path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Creperie::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( Creperie.js /batman/html)
+  config.assets.precompile += %w( creperie.js /batman/html)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
