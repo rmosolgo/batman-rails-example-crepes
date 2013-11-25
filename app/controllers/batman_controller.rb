2ebbc7b900579e6c4ebad26a16f39e56763399b6
Robert Mosolgo <rdmosolgo@gmail.com>
2013-11-25 13:06:37 -0800

    get batman-rails production code

--- app/controllers/batman_controller.rb
+++ app/controllers/batman_controller.rb
@@ -1,7 +1,12 @@
 class BatmanController < ApplicationController
 
   def index
-    render nothing: true, layout: 'batman'
+    if request.xhr?
+      prefix_length = Rails.application.config.assets.prefix.length + 1
+      path = request.path[prefix_length..-1]
+      render :text => Rails.application.assets[path]
+    else
+      render nothing: true, layout: 'batman'
+    end
   end
-
 end
